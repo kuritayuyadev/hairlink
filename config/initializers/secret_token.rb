e4b945b77aa1a9c697e67bf8804eecd3015adde8
Yuya Kurita <y.kurita@aktsk.jp>
2013-09-04 20:27:23 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hairlink::Application.config.secret_token = 'ebce5bad3fe8f1cb052be6917173877d3158eb68ef7df3b2099321f694d60e0f1ec9b953ec8955b655f41ca6676bc16e07c097ffcc2de732d62c27c3a1a89d89'
