786756c5edc6eb8ccfe22b38605f210b45a303ec
kurita <kurita.dev@gmail.com>
2013-09-14 01:18:33 +0900

    9-10まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
